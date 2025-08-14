0ef9567c3b3de761190047d3ef2aba080d278050
Josh Knopp <joshknopp@users.noreply.github.com>
2025-08-14 18:38:17 -0400

    fix: corner case for jaguars

--- api/src/modules/scraper/scraper.service.ts
+++ api/src/modules/scraper/scraper.service.ts
@@ -43,7 +43,7 @@ export class NflScraperService {
     GB: ['Packers', 'Green Bay Packers', 'Green Bay'],
     HOU: ['Texans', 'Houston Texans', 'Houston'],
     IND: ['Colts', 'Indianapolis Colts', 'Indianapolis'],
-    JAC: ['Jaguars', 'Jacksonville Jaguars', 'Jacksonville'],
+    JAC: ['Jaguars', 'Jacksonville Jaguars', 'Jacksonville', 'JAX'],
     KC: ['Chiefs', 'Kansas City Chiefs', 'Kansas City'],
     LV: ['Raiders', 'Las Vegas Raiders', 'Las Vegas'],
     LAC: ['Chargers', 'LA Chargers', 'Los Angeles Chargers', 'L.A. Chargers'],
